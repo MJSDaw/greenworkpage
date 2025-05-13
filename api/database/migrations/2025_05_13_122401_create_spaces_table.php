b5289fd335dfb1aaaa80aac04a16e1590dc73416
Victoria Real <victoriareall02@gmail.com>
2025-05-13 13:29:40 +0100

    space model, factory and migration added

--- api/database/migrations/2025_05_13_122401_create_spaces_table.php
+++ api/database/migrations/2025_05_13_122401_create_spaces_table.php
@@ -11,15 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('space', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('surname');
-            $table->string('dni')->unique();
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('places');
+            $table->float('price');
+            $table->string('schedule');
+            $table->string('images');
+            $table->string('description');
+            $table->string('subtitle');
             $table->timestamps();
         });
     }
@@ -29,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('spaces');
     }
 };
