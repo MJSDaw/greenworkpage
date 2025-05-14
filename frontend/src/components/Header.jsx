f0ca415f6c821aeef17215cc0000fed2337be506
Aileen <aileenpadrontorres511@.com>
2025-05-14 13:33:30 +0100

    Finished header but links not working

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,14 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from '../assets/img/logo.png'
+import menuHamburger from '../assets/img/menu_hamburguer.svg'
+
 
 const Header = () => {
+  const [menuActive, setMenuActive] = useState(false)
+
+  const toggleMenu = () => {
+    setMenuActive(!menuActive)
+  }
+  
   return (
     <header className='header__container'>
-      <img src={logo} alt='GreenWork Logo'/>
-      <nav>
-        <a href="#">Menú</a>
-        <a href="#">Registrarme</a>
-        <a href="#">Iniciar sesión</a>
+      <img src={logo} alt='GreenWork Logo' />
+      <nav className='nav__container'>
+        <div className={`nav__menu ${menuActive ? 'active' : ''}`}>
+          <a href="#">Spaces</a>
+          <a href="#">About</a>
+          <a href="#">Contact</a>
+          <a className='link__button--variant1' href="#">Sign in</a>
+          <a className='link__button' href="#">Login</a>
+        </div>
+        <button className={`nav__hamburger button--icon ${menuActive ? 'active' : ''}`} onClick={toggleMenu} aria-label="Open menu">
+          <img src={menuHamburger} alt="Hamburguer svg" className="nav__hamburguer__icon" />
+        </button>
       </nav>
     </header>
   )
