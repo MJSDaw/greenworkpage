e1ef7d5a65cc0af8f7c5cd7c9039b7f731869c59
Aileen <aileenpadrontorres511@.com>
2025-05-14 13:54:55 +0100

    Added links, have to fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
+import Home from './pages/Home'
+import Spaces from './pages/Spaces'
+import About from './pages/About'
+import Contact from './pages/Contact'
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
-    </>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/spaces" element={<Spaces />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </main>
+    </Router>
   )
 }
 
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 import logo from '../assets/img/logo.png'
 import menuHamburger from '../assets/img/menu_hamburguer.svg'
 
@@ -12,15 +13,17 @@ const Header = () => {
   
   return (
     <header className='header__container'>
-      <img src={logo} alt='GreenWork Logo' />
+      <Link to="/">
+        <img src={logo} alt='GreenWork Logo' />
+      </Link>
       <nav className='nav__container'>
-        <div className={`nav__menu ${menuActive ? 'active' : ''}`}>
-          <a href="#">Spaces</a>
-          <a href="#">About</a>
-          <a href="#">Contact</a>
-          <a className='link__button--variant1' href="#">Sign in</a>
-          <a className='link__button' href="#">Login</a>
-        </div>
+        <ul className={`nav__menu ${menuActive ? 'active' : ''}`}>
+          <li><Link to='/spaces'>Spaces</Link></li>
+          <li><Link to='/about'>About</Link></li>
+          <li><Link to='/contact'>Contact</Link></li>
+          <li><Link to='/signin'>Sign in</Link></li>
+          <li><Link to='/login'>Login</Link></li>
+        </ul>
         <button className={`nav__hamburger button--icon ${menuActive ? 'active' : ''}`} onClick={toggleMenu} aria-label="Open menu">
           <img src={menuHamburger} alt="Hamburguer svg" className="nav__hamburguer__icon" />
         </button>
