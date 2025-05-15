97a35f71ed681941e9b977ce996d22f996571b38
Aileen <aileenpadrontorres511@gmail.com>
2025-05-15 16:36:39 +0100

    Install prettier and linter

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -9,22 +9,77 @@ const Header = () => {
   const toggleMenu = () => {
     setMenuActive(!menuActive)
   }
-  
+
   return (
-    <header className='header__container'>
-      <Link to="/" title="Go to Home Page" className='header__logo'>
-        <img className='header__logo__icon' src={logo} alt='GreenWork' title="GreenWork Logo" />
+    <header className="header__container">
+      <Link to="/" title="Go to Home Page" className="header__logo">
+        <img
+          className="header__logo__icon"
+          src={logo}
+          alt="GreenWork"
+          title="GreenWork Logo"
+        />
       </Link>
-      <nav className='nav__container'>
+      <nav className="nav__container">
         <ul className={`nav__container__menu ${menuActive ? 'active' : ''}`}>
-          <li><Link to='/spaces' title="View coworking spaces" className='nav__container__menu__anchor'>Spaces</Link></li>
-          <li><Link to='/about' title="Learn more about GreenWork" className='nav__container__menu__anchor'>About</Link></li>
-          <li><Link to='/contact' title="Contact GreenWork" className='nav__container__menu__anchor'>Contact</Link></li>
-          <li className='nav__container__menu__anchor--button--variant'><Link to='/signin' title="Sign in to your account" className='nav__container__menu__anchor--button--variant__link'>Sign in</Link></li>
-          <li className='nav__container__menu__anchor--button'><Link to='/login' title="Login to your account" className='nav__container__menu__anchor--button__link'>Login</Link></li>
+          <li>
+            <Link
+              to="/spaces"
+              title="View coworking spaces"
+              className="nav__container__menu__anchor"
+            >
+              Spaces
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/about"
+              title="Learn more about GreenWork"
+              className="nav__container__menu__anchor"
+            >
+              About
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/contact"
+              title="Contact GreenWork"
+              className="nav__container__menu__anchor"
+            >
+              Contact
+            </Link>
+          </li>
+          <li className="nav__container__menu__anchor--button--variant">
+            <Link
+              to="/signin"
+              title="Sign in to your account"
+              className="nav__container__menu__anchor--button--variant__link"
+            >
+              Sign in
+            </Link>
+          </li>
+          <li className="nav__container__menu__anchor--button">
+            <Link
+              to="/login"
+              title="Login to your account"
+              className="nav__container__menu__anchor--button__link"
+            >
+              Login
+            </Link>
+          </li>
         </ul>
-        <button className={`nav__container__hamburger ${menuActive ? 'active' : ''}`} onClick={toggleMenu} aria-label="Open menu" title="Toggle navigation menu">
-          <img src={menuHamburger} alt="Hamburger menu icon" title="Toggle menu" className="nav__hamburguer__icon" />
+        <button
+          className={`nav__container__hamburger ${menuActive ? 'active' : ''}`}
+          onClick={toggleMenu}
+          aria-label="Open menu"
+          title="Toggle navigation menu"
+        >
+          <img
+            src={menuHamburger}
+            alt="Hamburger menu icon"
+            title="Toggle menu"
+            className="nav__hamburguer__icon"
+          />
         </button>
       </nav>
     </header>
