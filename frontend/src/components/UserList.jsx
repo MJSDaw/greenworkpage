3e4b16e0b3d1fc71f28ea0c417677399aabaec3c
Aileen <aileenpadrontorres511@gmail.com>
2025-05-20 09:44:47 +0100

    Edit

--- frontend/src/components/UserList.jsx
+++ frontend/src/components/UserList.jsx
@@ -8,89 +8,81 @@ import leonardo from '../assets/img/leonardo.svg'
 const UserList = () => {
   const { t } = useTranslation()
   const [formData, setFormData] = useState({
-      name: '',
-      surname: '',
-      birthdate: '',
-      dni: '',
-      email: '',
-      password: '',
-      passwordConfirm: '',
-      termsAndConditions: false,
-    })
+    name: '',
+    surname: '',
+    birthdate: '',
+    dni: '',
+    email: '',
+    password: '',
+    passwordConfirm: '',
+    termsAndConditions: false,
+  })
 
-  const [showForm, setShowForm] = useState(false);
-  const [showList, setShowList] = useState(true); // Mostrar lista por defecto
-  
-    const [errors, setErrors] = useState({})
-  
-    const handleChange = (e) => {
-      const { name, value, type, checked } = e.target
-      setFormData((prevData) => ({
-        ...prevData,
-        [name]: type === 'checkbox' ? checked : value,
-      }))
-    }
-  
-    const handleTermsChange = (e) => {
-      setFormData((prevData) => ({
-        ...prevData,
-        termsAndConditions: e.target.checked,
-      }))
-    }
+  const [showForm, setShowForm] = useState(false)
+  const [showList, setShowList] = useState(true)
+  const [errors, setErrors] = useState({})
 
-    const handleSubmit = async (e) => {
-      e.preventDefault()
-  
-      try {
-        const response = await fetch('/api/register', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(formData),
-        })
-        const data = await response.json()
-        console.log('Registration response:', data)
-  
-        if (data && data.success && data.token) {
-          setAuthToken(data.token, data.user)
-          setErrors({})
-          window.location.href = '/'
-        } else {
-          setErrors(data.errors || {})
-          console.log('Token not saved. Response data structure:', data)
-        }
-      } catch (error) {
-        console.error('Registration error:', error)
+  const handleChange = (e) => {
+    const { name, value, type, checked } = e.target
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: type === 'checkbox' ? checked : value,
+    }))
+  }
+
+  const handleTermsChange = (e) => {
+    setFormData((prevData) => ({
+      ...prevData,
+      termsAndConditions: e.target.checked,
+    }))
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    try {
+      const response = await fetch('/api/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      })
+      const data = await response.json()
+      console.log('Registration response:', data)
+
+      if (data && data.success && data.token) {
+        setAuthToken(data.token, data.user)
+        setErrors({})
+        window.location.href = '/'
+      } else {
+        setErrors(data.errors || {})
+        console.log('Token not saved. Response data structure:', data)
       }
+    } catch (error) {
+      console.error('Registration error:', error)
     }
+  }
 
-    const handleShowForm = () => {
-      setShowForm(true);
-      setShowList(false);
-    };
-  
-    const handleShowList = () => {
-      setShowForm(false);
-      setShowList(true);
-    };
-  
+  const handleShowForm = () => {
+    setShowForm(true)
+    setShowList(false)
+  }
+
+  const handleShowList = () => {
+    setShowForm(false)
+    setShowList(true)
+  }
 
   return (
     <>
       <h3>Usuarios</h3>
 
       <div className="user__buttons">
-        <button 
-          className="form__submit --noArrow" 
-          onClick={handleShowList}
-        >
+        <button className="form__submit --noArrow" onClick={handleShowList}>
           Ver usuarios
         </button>
-        <button 
-          className="form__submit --noArrow" 
-          onClick={handleShowForm}
-        >
+        <button className="form__submit --noArrow" onClick={handleShowForm}>
           Crear usuario
         </button>
       </div>
@@ -99,13 +91,39 @@ const UserList = () => {
         <section className="card__container">
           <article className="card">
             <div className="card__content">
-              <img src={leonardo} alt="" title="" className='card__img'/>
-              <div className='card__text'>
+              <img src={leonardo} alt={t('alt.dashboardImg')} title={t('common.dashboardImg')} className="card__img" />
+              <div className="card__text">
                 <p>Nombre</p>
                 <p>Correo</p>
               </div>
             </div>
-            <div className='card__buttons'>
+            <div className="card__buttons">
+              <button className="form__submit --noArrow">Editar</button>
+              <button className="form__submit --noArrow">Eliminar</button>
+            </div>
+          </article>
+          <article className="card">
+            <div className="card__content">
+              <img src={leonardo} alt={t('alt.dashboardImg')} title={t('common.dashboardImg')} className="card__img" />
+              <div className="card__text">
+                <p>Nombre</p>
+                <p>Correo</p>
+              </div>
+            </div>
+            <div className="card__buttons">
+              <button className="form__submit --noArrow">Editar</button>
+              <button className="form__submit --noArrow">Eliminar</button>
+            </div>
+          </article>
+          <article className="card">
+            <div className="card__content">
+              <img src={leonardo} alt={t('alt.dashboardImg')} title={t('common.dashboardImg')} className="card__img" />
+              <div className="card__text">
+                <p>Nombre</p>
+                <p>Correo</p>
+              </div>
+            </div>
+            <div className="card__buttons">
               <button className="form__submit --noArrow">Editar</button>
               <button className="form__submit --noArrow">Eliminar</button>
             </div>
@@ -113,188 +131,187 @@ const UserList = () => {
         </section>
       )}
 
-
-    {showForm && (
-      <section className="card__container--form">
-        <article className="card--form">
-          <form onSubmit={handleSubmit}>
-          <div className="form__section">
-            <label htmlFor="name">{t('form.name.label')}</label>
-            <input
-              id="name"
-              name="name"
-              placeholder={t('form.name.placeholder')}
-              value={formData.name}
-              onChange={handleChange}
-              required
-            />
-            {errors.name &&
-              Array.isArray(errors.name) &&
-              errors.name.map((err, idx) => (
-                <span className="form__error" key={idx}>
-                  {t(`errors.${err}`)}
-                </span>
-              ))}
-          </div>
-          <div className="form__section">
-            <label htmlFor="surname">{t('form.surname.label')}</label>
-            <input
-              id="surname"
-              name="surname"
-              placeholder={t('form.surname.placeholder')}
-              value={formData.surname}
-              onChange={handleChange}
-              required
-            />
-            {errors.surname &&
-              Array.isArray(errors.surname) &&
-              errors.surname.map((err, idx) => (
-                <span className="form__error" key={idx}>
-                  {t(`errors.${err}`)}
-                </span>
-              ))}
-          </div>
-          <div className="form__section">
-            <label htmlFor="birthdate">{t('form.birthday.label')}</label>
-            <input
-              id="birthdate"
-              name="birthdate"
-              placeholder={t('form.birthday.placeholder')}
-              type="date"
-              value={formData.birthdate}
-              onChange={handleChange}
-              required
-            />
-            {errors.birthdate &&
-              Array.isArray(errors.birthdate) &&
-              errors.birthdate.map((err, idx) => (
-                <span className="form__error" key={idx}>
-                  {t(`errors.${err}`)}
-                </span>
-              ))}
-          </div>
-          <div className="form__section">
-            <label htmlFor="nif">{t('form.nif.label')}</label>
-            <input
-              id="nif"
-              name="dni"
-              placeholder={t('form.nif.placeholder')}
-              value={formData.dni}
-              onChange={handleChange}
-              required
-            />
-            {errors.dni &&
-              Array.isArray(errors.dni) &&
-              errors.dni.map((err, idx) => (
-                <span className="form__error" key={idx}>
-                  {t(`errors.${err}`)}
-                </span>
-              ))}
-          </div>
-          <div className="form__section">
-            <label htmlFor="email">{t('form.email.label')}</label>
-            <input
-              id="email"
-              name="email"
-              placeholder={t('form.email.placeholder')}
-              value={formData.email}
-              onChange={handleChange}
-              required
-            />
-            {errors.email &&
-              Array.isArray(errors.email) &&
-              errors.email.map((err, idx) => (
-                <span className="form__error" key={idx}>
-                  {t(`errors.${err}`)}
-                </span>
-              ))}
-          </div>
-          <div className="form__section">
-            <label htmlFor="password">{t('form.password.label')}</label>
-            <input
-              id="password"
-              name="password"
-              type="password"
-              placeholder={t('form.password.placeholder')}
-              value={formData.password}
-              onChange={handleChange}
-              required
-            />
-            {errors.password &&
-              Array.isArray(errors.password) &&
-              errors.password.map((err, idx) => (
-                <span className="form__error" key={idx}>
-                  {t(`errors.${err}`)}
-                </span>
-              ))}
-          </div>
-          <div className="form__section">
-            <label htmlFor="confirmPassword">
-              {t('form.confirmPassword.label')}
-            </label>
-            <input
-              id="confirmPassword"
-              name="passwordConfirm"
-              type="password"
-              placeholder={t('form.confirmPassword.placeholder')}
-              value={formData.passwordConfirm}
-              onChange={handleChange}
-              required
-            />
-            {errors.confirmPassword &&
-              Array.isArray(errors.confirmPassword) &&
-              errors.confirmPassword.map((err, idx) => (
-                <span className="form__error" key={idx}>
-                  {t(`errors.${err}`)}
-                </span>
-              ))}
-          </div>
-          <div className="form__section">
-            <label className="input--checkbox__label">
+      {showForm && (
+        <section className="card__container--form">
+          <article className="card--form">
+            <form onSubmit={handleSubmit}>
+              <div className="form__section">
+                <label htmlFor="name">{t('form.name.label')}</label>
+                <input
+                  id="name"
+                  name="name"
+                  placeholder={t('form.name.placeholder')}
+                  value={formData.name}
+                  onChange={handleChange}
+                  required
+                />
+                {errors.name &&
+                  Array.isArray(errors.name) &&
+                  errors.name.map((err, idx) => (
+                    <span className="form__error" key={idx}>
+                      {t(`errors.${err}`)}
+                    </span>
+                  ))}
+              </div>
+              <div className="form__section">
+                <label htmlFor="surname">{t('form.surname.label')}</label>
+                <input
+                  id="surname"
+                  name="surname"
+                  placeholder={t('form.surname.placeholder')}
+                  value={formData.surname}
+                  onChange={handleChange}
+                  required
+                />
+                {errors.surname &&
+                  Array.isArray(errors.surname) &&
+                  errors.surname.map((err, idx) => (
+                    <span className="form__error" key={idx}>
+                      {t(`errors.${err}`)}
+                    </span>
+                  ))}
+              </div>
+              <div className="form__section">
+                <label htmlFor="birthdate">{t('form.birthday.label')}</label>
+                <input
+                  id="birthdate"
+                  name="birthdate"
+                  placeholder={t('form.birthday.placeholder')}
+                  type="date"
+                  value={formData.birthdate}
+                  onChange={handleChange}
+                  required
+                />
+                {errors.birthdate &&
+                  Array.isArray(errors.birthdate) &&
+                  errors.birthdate.map((err, idx) => (
+                    <span className="form__error" key={idx}>
+                      {t(`errors.${err}`)}
+                    </span>
+                  ))}
+              </div>
+              <div className="form__section">
+                <label htmlFor="nif">{t('form.nif.label')}</label>
+                <input
+                  id="nif"
+                  name="dni"
+                  placeholder={t('form.nif.placeholder')}
+                  value={formData.dni}
+                  onChange={handleChange}
+                  required
+                />
+                {errors.dni &&
+                  Array.isArray(errors.dni) &&
+                  errors.dni.map((err, idx) => (
+                    <span className="form__error" key={idx}>
+                      {t(`errors.${err}`)}
+                    </span>
+                  ))}
+              </div>
+              <div className="form__section">
+                <label htmlFor="email">{t('form.email.label')}</label>
+                <input
+                  id="email"
+                  name="email"
+                  placeholder={t('form.email.placeholder')}
+                  value={formData.email}
+                  onChange={handleChange}
+                  required
+                />
+                {errors.email &&
+                  Array.isArray(errors.email) &&
+                  errors.email.map((err, idx) => (
+                    <span className="form__error" key={idx}>
+                      {t(`errors.${err}`)}
+                    </span>
+                  ))}
+              </div>
+              <div className="form__section">
+                <label htmlFor="password">{t('form.password.label')}</label>
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  placeholder={t('form.password.placeholder')}
+                  value={formData.password}
+                  onChange={handleChange}
+                  required
+                />
+                {errors.password &&
+                  Array.isArray(errors.password) &&
+                  errors.password.map((err, idx) => (
+                    <span className="form__error" key={idx}>
+                      {t(`errors.${err}`)}
+                    </span>
+                  ))}
+              </div>
+              <div className="form__section">
+                <label htmlFor="confirmPassword">
+                  {t('form.confirmPassword.label')}
+                </label>
+                <input
+                  id="confirmPassword"
+                  name="passwordConfirm"
+                  type="password"
+                  placeholder={t('form.confirmPassword.placeholder')}
+                  value={formData.passwordConfirm}
+                  onChange={handleChange}
+                  required
+                />
+                {errors.confirmPassword &&
+                  Array.isArray(errors.confirmPassword) &&
+                  errors.confirmPassword.map((err, idx) => (
+                    <span className="form__error" key={idx}>
+                      {t(`errors.${err}`)}
+                    </span>
+                  ))}
+              </div>
+              <div className="form__section">
+                <label className="input--checkbox__label">
+                  <input
+                    className="input--checkbox"
+                    type="checkbox"
+                    name="termsAndConditions"
+                    checked={formData.termsAndConditions}
+                    onChange={handleTermsChange}
+                    required
+                  />
+                  <span className="input--checkbox__text">
+                    {t('form.checkbox.register.msg1')}
+                    <Link
+                      to="/terms"
+                      className="input--checkbox__text--link"
+                      title={t('actions.goToTerms')}
+                    >
+                      {t('links.terms')}
+                    </Link>{' '}
+                    {t('form.checkbox.register.msg2')}
+                    <Link
+                      to="/privacy"
+                      className="input--checkbox__text--link"
+                      title={t('actions.goToPrivacy')}
+                    >
+                      {t('links.privacy')}
+                    </Link>
+                  </span>
+                </label>
+                {errors.termsAndConditions &&
+                  Array.isArray(errors.termsAndConditions) &&
+                  errors.termsAndConditions.map((err, idx) => (
+                    <span className="form__error" key={idx}>
+                      {t(`errors.${err}`)}
+                    </span>
+                  ))}
+              </div>
               <input
-                className="input--checkbox"
-                type="checkbox"
-                name="termsAndConditions"
-                checked={formData.termsAndConditions}
-                onChange={handleTermsChange}
-                required
+                type="submit"
+                value="Crear usuario"
+                className="form__submit"
               />
-              <span className="input--checkbox__text">
-                {t('form.checkbox.register.msg1')}
-                <Link
-                  to="/terms"
-                  className="input--checkbox__text--link"
-                  title={t('actions.goToTerms')}
-                >
-                  {t('links.terms')}
-                </Link>{' '}
-                {t('form.checkbox.register.msg2')}
-                <Link
-                  to="/privacy"
-                  className="input--checkbox__text--link"
-                  title={t('actions.goToPrivacy')}
-                >
-                  {t('links.privacy')}
-                </Link>
-              </span>
-            </label>
-            {errors.termsAndConditions &&
-              Array.isArray(errors.termsAndConditions) &&
-              errors.termsAndConditions.map((err, idx) => (
-                <span className="form__error" key={idx}>
-                  {t(`errors.${err}`)}
-                </span>
-              ))}
-          </div>
-          <input
-            type="submit"
-            value='Crear usuario'
-            className="form__submit"
-          />
-        </form>
-        </article>
-      </section>
-    )}
+            </form>
+          </article>
+        </section>
+      )}
     </>
   )
 }
