dd5c3f4e7535303c87398bf3653d8d330dac556f
Aileen <aileenpadrontorres511@gmail.com>
2025-05-20 10:30:45 +0100

    not feched

--- frontend/src/components/UserList.jsx
+++ frontend/src/components/UserList.jsx
@@ -41,26 +41,39 @@ const UserList = () => {
     e.preventDefault()
 
     try {
-      const response = await fetch('/api/register', {
-        method: 'POST',
+      const url = editingId ? `/api/users/${editingId}` : '/api/register'
+      const method = editingId ? 'PUT' : 'POST'
+
+      const response = await fetch(url, {
+        method,
         headers: {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify(formData),
       })
       const data = await response.json()
-      console.log('Registration response:', data)
+      console.log(editingId ? 'Edit response:' : 'Registration response:', data)
 
-      if (data && data.success && data.token) {
-        setAuthToken(data.token, data.user)
-        setErrors({})
-        window.location.href = '/'
+      if (data && data.success) {
+        if (editingId) {
+          setEditingId(null)
+          setErrors({})
+          console.log('Usuario actualizado correctamente')
+        } else {
+          if (data.token) {
+            setAuthToken(data.token, data.user)
+            setErrors({})
+            window.location.href = '/'
+          } else {
+            setErrors(data.errors || {})
+            console.log('Token not saved. Response data structure:', data)
+          }
+        }
       } else {
         setErrors(data.errors || {})
-        console.log('Token not saved. Response data structure:', data)
       }
     } catch (error) {
-      console.error('Registration error:', error)
+      console.error(editingId ? 'Edit error:' : 'Registration error:', error)
     }
   }
 
@@ -72,6 +85,23 @@ const UserList = () => {
   const handleShowList = () => {
     setShowForm(false)
     setShowList(true)
+  }
+
+  const [editingId, setEditingId] = useState(null)
+
+  const handleEditClick = (id) => {
+    if (editingId === id) {
+      setEditingId(null)
+    } else {
+      setEditingId(id)
+      // Busca el usuario en tu lista y carga sus datos en el formulario
+      const userToEdit = users.find((user) => user.id === id)
+      setFormData({
+        name: userToEdit.name,
+        surname: userToEdit.surname,
+        // ... otros campos
+      })
+    }
   }
 
   return (
@@ -89,45 +119,180 @@ const UserList = () => {
 
       {showList && (
         <section className="card__container">
-          <article className="card">
-            <div className="card__content">
-              <img src={leonardo} alt={t('alt.dashboardImg')} title={t('common.dashboardImg')} className="card__img" />
-              <div className="card__text">
-                <p>Nombre</p>
-                <p>Correo</p>
-              </div>
-            </div>
-            <div className="card__buttons">
-              <button className="form__submit --noArrow">Editar</button>
-              <button className="form__submit --noArrow">Eliminar</button>
-            </div>
-          </article>
-          <article className="card">
-            <div className="card__content">
-              <img src={leonardo} alt={t('alt.dashboardImg')} title={t('common.dashboardImg')} className="card__img" />
-              <div className="card__text">
-                <p>Nombre</p>
-                <p>Correo</p>
-              </div>
-            </div>
-            <div className="card__buttons">
-              <button className="form__submit --noArrow">Editar</button>
-              <button className="form__submit --noArrow">Eliminar</button>
-            </div>
-          </article>
-          <article className="card">
-            <div className="card__content">
-              <img src={leonardo} alt={t('alt.dashboardImg')} title={t('common.dashboardImg')} className="card__img" />
-              <div className="card__text">
-                <p>Nombre</p>
-                <p>Correo</p>
-              </div>
-            </div>
-            <div className="card__buttons">
-              <button className="form__submit --noArrow">Editar</button>
-              <button className="form__submit --noArrow">Eliminar</button>
-            </div>
-          </article>
+          {users.map((user) => (
+            <React.Fragment key={user.id}>
+              <article className="card">
+                <div className="card__content">
+                  <img
+                    src={leonardo}
+                    alt={t('alt.dashboardImg')}
+                    title={t('common.dashboardImg')}
+                    className="card__img"
+                  />
+                  <div className="card__text">
+                    <p>{user.name}</p>
+                    <p>{user.email}</p>
+                  </div>
+                </div>
+                <div className="card__buttons">
+                  <button
+                    className="form__submit --noArrow"
+                    onClick={() => handleEditClick(user.id)}
+                  >
+                    Editar
+                  </button>
+                  <button className="form__submit --noArrow">Eliminar</button>
+                </div>
+              </article>
+
+              {editingId === user.id && (
+                <article className="card--form--edit">
+                  <form onSubmit={handleSubmit}>
+                    <div className="form__section">
+                      <label htmlFor="name">{t('form.name.label')}</label>
+                      <input
+                        id="name"
+                        name="name"
+                        placeholder={t('form.name.placeholder')}
+                        value={formData.name}
+                        onChange={handleChange}
+                        required
+                      />
+                      {errors.name &&
+                        Array.isArray(errors.name) &&
+                        errors.name.map((err, idx) => (
+                          <span className="form__error" key={idx}>
+                            {t(`errors.${err}`)}
+                          </span>
+                        ))}
+                    </div>
+                    <div className="form__section">
+                      <label htmlFor="surname">{t('form.surname.label')}</label>
+                      <input
+                        id="surname"
+                        name="surname"
+                        placeholder={t('form.surname.placeholder')}
+                        value={formData.surname}
+                        onChange={handleChange}
+                        required
+                      />
+                      {errors.surname &&
+                        Array.isArray(errors.surname) &&
+                        errors.surname.map((err, idx) => (
+                          <span className="form__error" key={idx}>
+                            {t(`errors.${err}`)}
+                          </span>
+                        ))}
+                    </div>
+                    <div className="form__section">
+                      <label htmlFor="birthdate">
+                        {t('form.birthday.label')}
+                      </label>
+                      <input
+                        id="birthdate"
+                        name="birthdate"
+                        placeholder={t('form.birthday.placeholder')}
+                        type="date"
+                        value={formData.birthdate}
+                        onChange={handleChange}
+                        required
+                      />
+                      {errors.birthdate &&
+                        Array.isArray(errors.birthdate) &&
+                        errors.birthdate.map((err, idx) => (
+                          <span className="form__error" key={idx}>
+                            {t(`errors.${err}`)}
+                          </span>
+                        ))}
+                    </div>
+                    <div className="form__section">
+                      <label htmlFor="nif">{t('form.nif.label')}</label>
+                      <input
+                        id="nif"
+                        name="dni"
+                        placeholder={t('form.nif.placeholder')}
+                        value={formData.dni}
+                        onChange={handleChange}
+                        required
+                      />
+                      {errors.dni &&
+                        Array.isArray(errors.dni) &&
+                        errors.dni.map((err, idx) => (
+                          <span className="form__error" key={idx}>
+                            {t(`errors.${err}`)}
+                          </span>
+                        ))}
+                    </div>
+                    <div className="form__section">
+                      <label htmlFor="email">{t('form.email.label')}</label>
+                      <input
+                        id="email"
+                        name="email"
+                        placeholder={t('form.email.placeholder')}
+                        value={formData.email}
+                        onChange={handleChange}
+                        required
+                      />
+                      {errors.email &&
+                        Array.isArray(errors.email) &&
+                        errors.email.map((err, idx) => (
+                          <span className="form__error" key={idx}>
+                            {t(`errors.${err}`)}
+                          </span>
+                        ))}
+                    </div>
+                    <div className="form__section">
+                      <label htmlFor="password">
+                        {t('form.password.label')}
+                      </label>
+                      <input
+                        id="password"
+                        name="password"
+                        type="password"
+                        placeholder={t('form.password.placeholder')}
+                        value={formData.password}
+                        onChange={handleChange}
+                        required
+                      />
+                      {errors.password &&
+                        Array.isArray(errors.password) &&
+                        errors.password.map((err, idx) => (
+                          <span className="form__error" key={idx}>
+                            {t(`errors.${err}`)}
+                          </span>
+                        ))}
+                    </div>
+                    <div className="form__section">
+                      <label htmlFor="confirmPassword">
+                        {t('form.confirmPassword.label')}
+                      </label>
+                      <input
+                        id="confirmPassword"
+                        name="passwordConfirm"
+                        type="password"
+                        placeholder={t('form.confirmPassword.placeholder')}
+                        value={formData.passwordConfirm}
+                        onChange={handleChange}
+                        required
+                      />
+                      {errors.confirmPassword &&
+                        Array.isArray(errors.confirmPassword) &&
+                        errors.confirmPassword.map((err, idx) => (
+                          <span className="form__error" key={idx}>
+                            {t(`errors.${err}`)}
+                          </span>
+                        ))}
+                    </div>
+                    <input
+                      type="submit"
+                      value="Editar usuario"
+                      className="form__submit"
+                    />
+                  </form>
+                </article>
+              )}
+            </React.Fragment>
+          ))}
         </section>
       )}
 
