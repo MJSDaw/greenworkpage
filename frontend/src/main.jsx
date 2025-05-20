e73f03ff43b7e5cecf18d47493a11fbdd3847f1b
Moises Santana <mjsdaw@gmail.com>
2025-05-20 17:31:59 +0100

    cleaning fetch inspector

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,12 +3,8 @@ import './index.css'
 import './locales/i18n';
 import { React, StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { setupFetchInterceptor } from './services/fetchInterceptor'
 
 import App from './App.jsx'
-
-// Setup fetch interceptor to handle 401 responses globally
-setupFetchInterceptor();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
