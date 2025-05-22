754cb7c2e01e98b082b9df9ecaf30c39459969e7
Aileen <aileenpadrontorres511@gmail.com>
2025-05-22 13:50:34 +0100

    Space details

--- frontend/src/pages/Space.jsx
+++ frontend/src/pages/Space.jsx
@@ -2,8 +2,15 @@ import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import ContactUs from '../components/ContactUs'
 import { getSpaceById } from '../services/apiService'
+import { useTranslation } from 'react-i18next'
+import ServiceCard from '../components/ServiceCard'
+
+import pc from '../assets/img/pc.svg'
+import maps from '../assets/img/maps.svg'
+import eur from '../assets/img/eur.svg'
 
 const Space = () => {
+  const { t } = useTranslation()
   const { id } = useParams()
   const [space, setSpace] = useState([])
   const [loading, setLoading] = useState(true)
@@ -20,6 +27,7 @@ const Space = () => {
             amount: response.data.price,
             maps: response.data.address || '',
             seats: response.data.places,
+            description: response.data.description,
           }
           setSpace(spacesData)
         }
@@ -33,12 +41,52 @@ const Space = () => {
   }, [])
 
   return (
-    <div>
-      <h1>SPACE {id}</h1>
-      <img src={space.src} alt="" />
-      <ContactUs />
-    </div>
-  )
+  <>
+    <main className="terms__background--space">
+      <section className="terms">
+        <h1 className="h1--left">
+          {t('form.space.label')} {space.subtitle}
+        </h1>
+        <article className="space__grid">
+          <img
+            src={space.src}
+            alt={space.subtitle}
+            className="space__img"
+          />
+          <div className="space__features">
+            <h2>{t('common.features')}</h2>
+            <div className="feature__item">
+              <img src={pc} alt="Seats icon" />
+              <span>{space.seats} {t('form.seats.label')}</span>
+            </div>
+            <div className="feature__item">
+              <img src={maps} alt="Location icon" />
+              <span>{space.maps}</span>
+            </div>
+            <div className="feature__item">
+              <img src={eur} alt="Price icon" />
+              <span>{space.amount} / {t('form.time.label')}</span>
+            </div>
+          </div>
+        </article>
+        <article className='space__services'>
+          <ServiceCard src='' text='' />
+          <ServiceCard />
+          <ServiceCard />
+          <ServiceCard />
+          <ServiceCard />
+          <ServiceCard />
+          <ServiceCard />
+        </article>
+        <article className='space__description'>
+          <h2>{t('form.description.label')}</h2>
+          <p>{space.description}</p>
+        </article>
+      </section>
+    </main>
+    <ContactUs />
+  </>
+);
 }
 
 export default Space
