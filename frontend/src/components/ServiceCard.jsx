f358fe89a7b979b89de9a883e6041769bef76f5e
Moises Santana <mjsdaw@gmail.com>
2025-05-22 19:01:15 +0100

    temp limited size

--- frontend/src/components/ServiceCard.jsx
+++ frontend/src/components/ServiceCard.jsx
@@ -1,7 +1,7 @@
 const ServiceCard = ({ src, title }) => {
   return (
     <div className="service__card">
-      <img src={`https://localhost:8443/storage/${src}`} alt={title} />
+      <img src={`https://localhost:8443/storage/${src}`} alt={title} style={{width: '50px'}} />
       <span>{title}</span>
     </div>
   )
